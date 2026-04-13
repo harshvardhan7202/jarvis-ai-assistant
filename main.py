3f54d94beabf4846a16b9d3a6ab46762d73ca0a3
harshvardhan7202 <harshvardhansingh262006@gmail.com>
2026-04-13 18:57:53 +0530

    Updated Jarvis project

--- main.py
+++ main.py
@@ -1,59 +1,145 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
-import musicLibrary
-
-# pip install pocketsphinx
+import requests
+from datetime import datetime
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
 
 def speak(text):
+    print("Jarvis:", text)   
     engine.say(text)
     engine.runAndWait()
 
 def processCommand(c):
-    if "open google" in c.lower():
+    print("Command received:", c) 
+
+    if "open google" in c.lower():    # Open Google
+        speak("Opening Google")
         webbrowser.open("https://google.com")
-    elif "open youtube" in c.lower():
+
+    elif "open youtube" in c.lower():   # Open YT
+        speak("Opening YouTube")
         webbrowser.open("https://youtube.com")
-    elif "open facebook" in c.lower():
-        webbrowser.open("https://facebook.com")
-    elif "open instagram" in c.lower(): 
+
+    elif "open linkedin" in c.lower(): # Open LinkedIn
+        speak("Opening Linkedin")
+        webbrowser.open("https://www.linkedin.com/feed/")
+
+    elif "open instagram" in c.lower():  # Open Insta
+        speak("Opening Instagram")
         webbrowser.open("https://instagram.com")
-    elif c.lower().startswith("play"):
-        song = c.lower().split(" ")[1]
-        link = musicLibrary.music[song]
-        webbrowser.open(link)
+
+    elif c.lower().startswith("play"): # Play song on Spotify
+        if ("spotify") in c.lower():
+            song = c.lower().replace("play", "").strip()
+            song = song.lower().replace("on spotify", "").strip()
+            webbrowser.open(f"https://open.spotify.com/search/{song}")
+        
+        else:
+            song = c.lower().replace("play", "").strip()
+            webbrowser.open(f"https://open.spotify.com/search/{song}")
+
+    elif c.lower().startswith("search"): # Search on YT
+        if("youtube") in c.lower():
+            video = c.lower().replace("search", "").strip()
+            video = video.lower().replace("on youtube", "").strip()
+            webbrowser.open(f"https://www.youtube.com/results?search_query={video}")
+
+        else:
+            video = c.lower().replace("search", "").strip()
+            webbrowser.open(f"https://www.youtube.com/results?search_query={video}")
+
+    elif "tell me score" in c.lower(): # Cricket match score
+
+        speak("Fetching latest score")
+
+        url = "https://api.cricapi.com/v1/currentMatches?apikey=5ab0ce63-45b0-4a13-9d7a-472445d3864e"
+    
+        try:
+            response = requests.get(url)
+            data = response.json()
+
+            # Take first match
+            match = data["data"][0]
+
+            team1 = match["teamInfo"][0]["shortname"]
+            team2 = match["teamInfo"][1]["shortname"]
+
+            score1 = match["score"][0]
+            runs = score1["r"]
+            wickets = score1["w"]
+
+            status = match["status"]
+
+            result = f"{team1} scored {runs} for {wickets}. {status}"
+
+            print(result)
+            speak(result)
+
+        except Exception as e:
+            print("Error:", e)
+            speak("Sorry, I could not fetch the score")
+
+    elif "tell me time" in c.lower():  # Time
+        current_time = datetime.now().strftime("%H:%M")
+        speak(f"The time is {current_time}")
+
+    elif "tell me weather" in c.lower(): # Weather
+
+        speak("Fetching weather")
+
+        url = "https://api.openweathermap.org/data/2.5/weather?q=Jaipur&appid=4a1939b340bb6e17bc6cefb9797335a2&units=metric"
+
+        response = requests.get(url)
+        data = response.json()
+
+        temp = data["main"]["temp"]
+        condition = data["weather"][0]["description"]
+
+        result = f"The temperature is {temp} degrees and weather is {condition}"
+
+        print(result)
+        speak(result)
+
+    elif "who are you" in c.lower():  # Introduction
+        speak("I am Jarvis, your personal voice assistant. I can help you with tasks like playing music, telling time, and more.")
+
+    elif "who created you" in c.lower():  # Creator Intro
+        speak("I was created by Harshvardhan Singh, a B.Tech Computer Science student in AI and ML at JECRC University, as part of his learning and development in AI projects.")
 
 
 
 if __name__ == "__main__":
     speak("Initializing Jarvis.....")
-    while True:
-        # Listen for the wake word "Jarvis"
-        # obtain audio from the microphone
-        recognizer = sr.Recognizer() # Source : Speech Recognition GitHub
 
-        print("recognizing...")
+    while True:
+        recognizer = sr.Recognizer()
+
+        print("Recognizing...")   
+
         try:
             with sr.Microphone() as source:
                 recognizer.adjust_for_ambient_noise(source)
-                print("Listening...")
-                audio = recognizer.listen(source, timeout=2, phrase_time_limit=1)
+                print("Listening for wake word...")
+                audio = recognizer.listen(source, timeout=3, phrase_time_limit=3)
+
             word = recognizer.recognize_google(audio)
+            print("You said :", word)  
 
             if(word.lower() == "jarvis"):
                 speak("Yes")
-                # Listen for command
+
                 with sr.Microphone() as source:
                     recognizer.adjust_for_ambient_noise(source)
-                    print("Jarvis Active...")
+                    print("Jarvis Active... Listening for command...")
                     audio = recognizer.listen(source)
+
                     command = recognizer.recognize_google(audio)
+                    print("You said (command):", command)   
 
                     processCommand(command)
 
-
         except Exception as e:
-            print("Error; {0}".format(e)) # Source : Speech Recognition GitHub
+            print("Error:", e)
--- musicLibrary.py
+++
@@ -1,6 +0,0 @@
-music = {
-    "starboy":"https://www.youtube.com/watch?v=34Na4j8AVgA&pp=ygUHc3RhcmJvedIHCQnbCgGHKiGM7w%3D%3D",
-    "brooklyn":"https://www.youtube.com/watch?v=T5xcnjAG8pE&pp=ygUaYnJvb2tseW4gYmFieSBsYW5hIGRlbCByZXk%3D",
-    "lights":"https://www.youtube.com/watch?v=4NRXx6U8ABQ&pp=ygUaYmxpbmRpbmcgbGlnaHRzIHRoZSB3ZWVrbmQ%3D",
-    "nights":"https://www.youtube.com/watch?v=UtF6Jej8yb4&pp=ygURdGhlIG5pZ2h0cyBhdmljaWk%3D"
-}
